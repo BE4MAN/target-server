e4be1577cda21d4cfe759cdfa7db787bd8088ce6
Heo-Gyeom <heogyeom80@gmail.com>
2025-11-21 17:29:19 +0900

    수정

--- src/main/java/com/target/global/controller/HelloController.java
+++ src/main/java/com/target/global/controller/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello target server ~~1l1l1123!!!!!!";
+        return "Hello target server ~~1l1l1!!!!!";
     }
 }
