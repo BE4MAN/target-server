ba0f6963e0f10345968f084fdd8383a63dfea1dc
kmdev <kmdev0494@gmail.com>
2025-11-21 14:26:53 +0900

    chore: hello controller 수정

--- src/main/java/com/target/global/controller/HelloController.java
+++ src/main/java/com/target/global/controller/HelloController.java
@@ -6,9 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-
     @GetMapping("/")
     public String hello() {
-        return "Hello target server ~~1l1l1l!!!!!!";
+        return "Hello target server ~~!!!!!!";
     }
 }
