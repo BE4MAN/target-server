0b2bb9499c94782326e625de850fd02e76c13c35
kmdev-04 <kmdev0494@gmail.com>
2026-02-11 14:20:11 +0900

    Update HelloController.java

--- src/main/java/com/target/global/controller/HelloController.java
+++ src/main/java/com/target/global/controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello target server ~~!!!!!!!!!!11123313";
+        return "Hello target server ~~!!!!!!!!!!1112331311";
     }
 
 
